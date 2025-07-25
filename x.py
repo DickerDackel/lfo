1f31270eeadbcb6f7ac2a9c04e99e3146b76b6d3
Michael Lamertz <michael.lamertz@gmail.com>
2025-07-25 15:41:37 +0200

    Functionality tests while no pytest exists and I'm working on the core

--- x.py
+++ x.py
@@ -11,11 +11,18 @@ FPS = 60
 DT_MAX = 3 / FPS
 
 clock = pygame.time.Clock()
-window = pygame.Window(title=TITLE, size=SCREEN.size)
+window = pygame.Window(title=TITLE, fullscreen_desktop=True)
 renderer = sdl2.Renderer(window)
+renderer.logical_size = SCREEN.size
 
 RADIUS = 256
-lfo = LFO(2, sine_offset=0.0, cosine_offset=0.0)
+SATTELITE_RADIUS = 32
+
+orbit = LFO(10, sine_offset=0.0, cosine_offset=0.0)
+sattelite = LFO(5, sine_offset=0.0, cosine_offset=0.0)
+speedo = LFO(20, sine_attenuverter=0.3, sine_offset=1.0, cosine_attenuverter=0.3, cosine_offset=1.0)
+color = LFO(3)
+
 rect = pygame.Rect(0, 0, 10, 10)
 
 running = True
@@ -32,14 +39,27 @@ while running:
     renderer.draw_color = 'darkslategrey'
     renderer.clear()
 
-    renderer.draw_color = 'red'
+    renderer.draw_color = ('red', 'green')[int(color.square)]
+
     renderer.draw_rect(rect.move_to(center=SCREEN.center))
 
-    print(lfo.sine)
-    x = lfo.cosine * RADIUS + SCREEN.centerx
-    y = lfo.sine * RADIUS + SCREEN.centery
-    renderer.draw_color = 'yellow'
-    renderer.draw_rect(rect.move_to(center=(x, y)))
+    x = orbit.cosine * RADIUS + SCREEN.centerx
+    y = orbit.sine * RADIUS + SCREEN.centery
+    renderer.draw_color = ('yellow', 'magenta')[int(color.square)]
+    renderer.draw_rect(rect.move_to(center=(round(x), round(y))))
+
+    x += sattelite.cosine * SATTELITE_RADIUS
+    y += sattelite.sine * SATTELITE_RADIUS
+    renderer.draw_color = ('cyan', 'orange')[int(color.square)]
+    renderer.draw_rect(rect.move_to(center=(round(x), round(y))))
+
+    sattelite.period = speedo.sine
+
+    x = orbit.sine * 2 * RADIUS + SCREEN.centerx + sattelite.sine * SATTELITE_RADIUS
+    y = orbit.cosine * 2 * RADIUS + SCREEN.centery + sattelite.cosine * SATTELITE_RADIUS
+    renderer.draw_color = ('green', 'red')[int(color.square)]
+    renderer.draw_rect(rect.scale_by(2).move_to(center=(round(x), round(y))))
+
 
     renderer.present()
 
